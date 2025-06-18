bf2aac254b510d95905bba0dccbb2dde154abd79
rahul <chrahulofficial@gmail.com>
2025-06-18 20:03:20 +0530

    fixed

--- out/next/static/NO6o3cWigwY5iHdx8LYWL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/257e8032-24353df26aa3e479.js","static/chunks/434-8aff8afd9131a6b1.js","static/chunks/pages/index-acb540357d0a00b3.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
