0c03247684e3f2a3c3106cd64adfdce04546e401
Chrahuldeveloper <chrahulofficial@gmail.com>
2025-07-12 13:52:38 +0530

    rollback

---
+++ out/next/static/chunks/68c0a17d-86014d1df3aed5f7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[660],{8318:function(t,n,e){e.d(n,{uCd:function(){return i}});var r=e(5054);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"32",d:"M176 96h16v320h-16zm144 0h16v320h-16z"},child:[]}]})(t)}}}]);
